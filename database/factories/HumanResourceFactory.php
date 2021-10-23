a357097f60174baf99be52b9aada87b8211631d1
willyishmael <wtangka22@gmail.com>
2021-10-23 17:46:27 +0800

    Update HumanResourceFactory.php

--- database/factories/HumanResourceFactory.php
+++ database/factories/HumanResourceFactory.php
@@ -7,22 +7,17 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 
 class HumanResourceFactory extends Factory
 {
-    /**
-     * The name of the factory's corresponding model.
-     *
-     * @var string
-     */
     protected $model = human_resource::class;
 
-    /**
-     * Define the model's default state.
-     *
-     * @return array
-     */
     public function definition()
     {
         return [
-            //
+            'name' => $this->faker->name(),
+            'jenis_kelamin' => $this->faker->randomElement(['Laki-laki','Perempuan']),
+            'nip' => $this->faker->numberBeetwen(1000000000, 9999999999),
+            'tanggal_lahir' => $this->faker->date($max = '1995-12-31'),
+            'peran' => $this->faker->randomElement(['Guru','Staf Tata Usaha', ]),
+            'foto' => $this->faker->imageUrl($category = 'people')
         ];
     }
 }

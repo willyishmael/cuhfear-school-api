8e56dbf62feb4b36b875ce524a4090b678a005b9
willyishmael <wtangka22@gmail.com>
2021-10-24 19:25:18 +0800

    Update HumanResourceController.php

--- app/Http/Controllers/HumanResourceController.php
+++ app/Http/Controllers/HumanResourceController.php
@@ -35,7 +35,21 @@ class HumanResourceController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validated = $request->validate([
+            'nama' => 'required|max:50',
+            'jenis_kelamin' => 'required|max:20',
+            'nip' => 'max:20|nullable',
+            'tanggal_lahir' => 'required|date',
+            'peran' => 'required|max:20',
+            'jabatan' => 'max:20|nullabel',
+            'foto' => 'image|nullable'
+        ]);
+
+        HumanResource::create($validated);
+
+        return response()->json([
+            'message' => 'new data has been added to HumanResources'
+        ]);
     }
 
     /**
@@ -46,7 +60,9 @@ class HumanResourceController extends Controller
      */
     public function show(HumanResource $human_resource)
     {
-        //
+        return response()->json([
+            'human_resource' => $human_resource->all()
+        ]);
     }
 
     /**
@@ -67,9 +83,32 @@ class HumanResourceController extends Controller
      * @param  \App\Models\human_resource  $human_resource
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, HumanResource $human_resource)
+    public function update(Request $request, HumanResource $human_resource, $id)
     {
-        //
+        $validated = $request->validate([
+            'nama' => 'required|max:50',
+            'jenis_kelamin' => 'required|max:20',
+            'nip' => 'max:20|nullable',
+            'tanggal_lahir' => 'required|date',
+            'peran' => 'required|max:20',
+            'jabatan' => 'max:20|nullabel',
+            'foto' => 'image|nullable'
+        ]);
+
+        $hr = $human_resource->find($id);
+        $hr->nama = $validated['nama'];
+        $hr->jenis_kelamin = $validated['jenis_kelamin'];
+        $hr->nip = $validated['nip'];
+        $hr->tanggal_lahir = $validated['tanggal_lahir'];
+        $hr->peran = $validated['peran'];
+        $hr->jabatan = $validated['jabatan'];
+        $hr->foto = $validated['foto'];
+        $hr->save();
+
+        return response()->json([
+            'message' => 'new data has been added to HumanResources',
+            'updated_data' => $hr
+        ]);
     }
 
     /**
@@ -78,8 +117,13 @@ class HumanResourceController extends Controller
      * @param  \App\Models\human_resource  $human_resource
      * @return \Illuminate\Http\Response
      */
-    public function destroy(HumanResource $human_resource)
+    public function destroy(HumanResource $human_resource, $id)
     {
-        //
+        $hr = $human_resource->find($id);
+        $hr->delete();
+
+        return response()->json([
+            'message' => 'data has been deleted from HumanResources',
+        ]);
     }
 }

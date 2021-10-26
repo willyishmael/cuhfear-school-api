4d41a5f28e65016cdf2a7c39d9ae73556de464c0
willyishmael <wtangka22@gmail.com>
2021-10-26 19:36:31 +0800

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,22 +9,43 @@ use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    public function index(Request $request)
+    public function login(Request $request)
     {
         $user = User::where('email', $request->email)->first();
         if (!$user || !Hash::check($request->password, $user->password)) {
             return response([
-                'message' => ['These credentials do not match our records.']
+                'message' => 'These credentials do not match our records.'
             ], 404);
         }
 
         $token = $user->createToken('my-app-token')->plainTextToken;
 
         $response = [
+            'message' => 'Login successful',
             'user' => $user,
             'token' => $token
         ];
 
-        return response($response, 201);
+        return response()->json($response);
+    }
+
+    public function logout(Request $request)
+    {
+        $user = $request->user();
+        $user->tokens()->where('id', $request->user()->currentAccessToken()->id)->delete();
+
+        $response = [
+            'message' => 'Logout successful',
+            'user' => $user
+        ];
+
+        return response()->json($response);
+    }
+
+    public function user(Request $request)
+    {
+        $user = $request->user();
+
+        return response()->json($user);
     }
 }

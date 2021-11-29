e572ea08396db9f397bc308495cc67a0915512d1
willyishmael <wtangka22@gmail.com>
2021-11-29 14:57:30 +0800

    Update StudentRegistrationRequest.php

--- app/Http/Requests/StudentRegistrationRequest.php
+++ app/Http/Requests/StudentRegistrationRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class StudentRegistrationRequest extends FormRequest
 {
@@ -23,8 +24,29 @@ class StudentRegistrationRequest extends FormRequest
      */
     public function rules()
     {
+        $jalur = ['zonasi','afirmasi','prestasi','perpindahan_tugas'];
+        $jenis_kelamin = ['laki-laki', 'perempuan'];
+
         return [
-            //
+            'jalur_pendaftaran' => ['required', Rule::in($jalur)],
+            'nama' => 'required|max:50',
+            'jenis_kelamin' => ['required', Rule::in($jenis_kelamin)],
+            'tempat_lahir' => 'required|max:20',
+            'tanggal_lahir' => 'required|date',
+            'alamat' => 'required',
+            'email' => 'required|email',
+            'nomor_telepon' => 'required',
+            'nama_wali' => 'required|max:50',
+            'nomor_telepon_wali' => 'required',
+            'asal_sekolah' => 'required',
+            'kartu_keluarga' => 'required|file|max:2048',
+            'sc_jarak_rumah' => 'nullable|image|file|max:1024',
+            'sk_domisili' => 'nullable|file|max:2048',
+            'sk_lulus' => 'required|file|max:2048',
+            'sk_tidak_mampu' => 'nullable|file|max:2048',
+            'sk_pindah_tugas' => 'nullable|file|max:2048',
+            'sertifikat_prestasi' => 'nullable|file|max:2048',
+            'sp_keabsahan_dokumen' => 'nullable|file|max:2048',
         ];
     }
 }
